6ed6f3ddce5a732f779f5f3dad13d6d743c1434c
Diego Sarmiento <dasa1088@gmail.com>
2024-03-15 09:48:19 -0400

    Connection WeDev survery site MongoDB

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
+// Import
+const config = require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
-app.listen(3000, () => {
-        console.log('Server is running on port 3000');
+// Set up mongoose
+mongoose.Promise = global.Promise;
+
+// Connect to the database
+mongoose.connect(config.mongoUri).then(() => {
+    console.log("Connected to WeDev Survey database!");
+});
+    
+mongoose.connection.on('error', () => {
+    throw new Error(`unable to connect to database: ${config.mongoUri}`) 
 });
 
-app.get('/', (req, res) => {
-        res.send('Welcome WeDev web site!');
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome WeDev web site!" });
+});
+
+app.listen(config.port, (err) => { 
+    if (err) {
+        console.log(err) 
+    }
+    console.info('Server is running on port %s.', config.port) 
 });
